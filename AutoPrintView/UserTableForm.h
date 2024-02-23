260e7b1ce3c460c8d55bf886cae4b2faa539be98
Alexander Faridh Tumba Castillo <faridh20000@outlook.com>
2024-02-23 16:42:10 -0500

    vamooo x2

--- AutoPrintView/UserTableForm.h
+++ AutoPrintView/UserTableForm.h
@@ -798,7 +798,7 @@ namespace AutoPrintView {
 			   Random^ rnd = gcnew Random();
 			   while (true) {
 				   try {
-					   myThread->Sleep((rnd->Next() % 5 + 1) * 1000);
+					   myThread->Sleep(30000);
 					   Invoke(gcnew MyDelegate(this, &UserTableForm::RefreshGrid));
 				   }
 				   catch (Exception^ ex) {
